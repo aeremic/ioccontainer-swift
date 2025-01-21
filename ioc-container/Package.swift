16e6db75bdf60d0f2ab56fd5cc79cc6289721713
aeremic <andrija.eremic@gmail.com>
2025-01-21 19:42:46 +0100

    package fix

--- ioc-container/Package.swift
+++ ioc-container/Package.swift
@@ -9,15 +9,15 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ioc-container",
-            targets: ["ioc-container"]),
+            targets: ["ioc-container"])
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ioc-container"),
+            name: "ioc-container/Sources/ioc-container"),
         .testTarget(
             name: "ioc-containerTests",
-            dependencies: ["ioc-container"]),
+            dependencies: ["ioc-container/Tests/ioc-containerTests"]),
     ]
 )
